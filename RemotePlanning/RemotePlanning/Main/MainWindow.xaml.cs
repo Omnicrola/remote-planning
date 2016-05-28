3b87700040fd293e7327ca4f70bee1b52265669c
Eric Schreffler <omnicrola@gmail.com>
2016-05-28 18:46:17 -0400

    Button to add a planning sheet now adds a sheet to the currently selected Iteration

--- RemotePlanning/RemotePlanning/Main/MainWindow.xaml.cs
+++ RemotePlanning/RemotePlanning/Main/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using RemotePlanning.Iteration;
 using RemotePlanning.Main;
@@ -15,12 +18,15 @@ namespace RemotePlanning
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IterationViewModel _previouslySelectedIteration;
         public MainWindowViewModel ViewModel { get; private set; }
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = ViewModel = new MainWindowViewModel(new CanvasZoomViewModel());
+            ViewModel.ZoomViewModel.ScaleX = 0.5;
+            ViewModel.ZoomViewModel.ScaleY = 0.5;
             ViewModel.PropertyChanged += SelectedIteration_PropertyChange;
         }
 
@@ -28,13 +34,29 @@ namespace RemotePlanning
         {
             if (e.PropertyName.Equals("SelectedIteration"))
             {
+                if (_previouslySelectedIteration != null) { _previouslySelectedIteration.PlanningSheets.CollectionChanged -= PlanningSheets_OnChange; }
                 GameCanvas.Children.Clear();
+                ViewModel.SelectedIteration.PlanningSheets.CollectionChanged += PlanningSheets_OnChange;
                 foreach (PlanningSheetViewModel planningSheetViewModel in ViewModel.SelectedIteration.PlanningSheets)
                 {
-                    GameCanvas.Children.Add(new PlanningSheetControl() { DataContext = planningSheetViewModel });
+                    AddPlanningSheet(planningSheetViewModel);
                 }
-
+                _previouslySelectedIteration = ViewModel.SelectedIteration;
             }
+        }
+
+        private void PlanningSheets_OnChange(object sender, NotifyCollectionChangedEventArgs e)
+        {
+
+            e.NewItems.Cast<PlanningSheetViewModel>().ToList().ForEach(AddPlanningSheet);
+        }
+
+        private void AddPlanningSheet(PlanningSheetViewModel planningSheetViewModel)
+        {
+            var planningSheetControl = new PlanningSheetControl() { DataContext = planningSheetViewModel };
+            Canvas.SetTop(planningSheetControl, new Random().Next(10, 500));
+            Canvas.SetLeft(planningSheetControl, new Random().Next(10, 500));
+            GameCanvas.Children.Add(planningSheetControl);
         }
 
         private void Canvas_OnMouseWheel(object sender, MouseWheelEventArgs mouseArgs)
@@ -63,6 +85,11 @@ namespace RemotePlanning
         private void ChangeSelectedIteration(object sender, SelectIterationEventArgs eventArgs)
         {
             ViewModel.SelectedIteration = eventArgs.SelectedIteration;
+        }
+
+        private void AddSheet_OnClick(object sender, RoutedEventArgs e)
+        {
+            ViewModel.SelectedIteration.PlanningSheets.Add(new PlanningSheetViewModel());
         }
     }
 }
