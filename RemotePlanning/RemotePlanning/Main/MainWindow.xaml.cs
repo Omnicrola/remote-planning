9f01cdb6fb95e94b7915ac2bfc7bc8da5187e7d3
Eric Schreffler <omnicrola@gmail.com>
2016-05-28 13:21:44 -0400

    User can now zoom the main canvas

---
+++ RemotePlanning/RemotePlanning/Main/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using RemotePlanning.Main;
+
+namespace RemotePlanning
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _mainWindowViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _mainWindowViewModel = new MainWindowViewModel();
+        }
+
+        private void Canvas_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            _mainWindowViewModel.AdjustScale(e.Delta);
+        }
+    }
+}
---
+++ RemotePlanning/RemotePlanning/Main/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using RemotePlanning.ViewModels;
+
+namespace RemotePlanning.Main
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private const double SCALE_RATE = 1.1;
+
+        private double _scaleX;
+        private double _scaleY;
+
+        public MainWindowViewModel()
+        {
+            ScaleX = 1;
+            ScaleY = 1;
+        }
+
+        public double ScaleX
+        {
+            get { return _scaleX; }
+            set { SetPropertyField(ref _scaleX, value); }
+        }
+
+        public double ScaleY
+        {
+            get { return _scaleY; }
+            set { SetPropertyField(ref _scaleY, value); }
+        }
+
+        public void AdjustScale(int delta)
+        {
+            Console.WriteLine("Delta: " + delta);
+            if (delta > 0)
+            {
+                ScaleX *= SCALE_RATE;
+                ScaleY *= SCALE_RATE;
+            }
+            else
+            {
+                ScaleX /= SCALE_RATE;
+                ScaleY /= SCALE_RATE;
+            }
+            Console.WriteLine($"X: {ScaleX} Y:{ScaleY}");
+        }
+    }
+}
