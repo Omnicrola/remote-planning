9ec6a07b82dd66387538dfd63b9a380d53f5fc32
Eric Schreffler <omnicrola+tards@gmail.com>
2016-06-01 21:54:26 -0400

    Created a reusuable control that allows some text to be edited when it is double-clicked

---
+++ RemotePlanning/RemotePlanning/Ui/EditableLabel.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace RemotePlanning.Ui
+{
+    /// <summary>
+    /// Interaction logic for EditableLabel.xaml
+    /// </summary>
+    public partial class EditableLabel : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _isEditing;
+        private string _changedText;
+
+        public EditableLabel()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string Text
+        {
+            get { return (string)base.GetValue(TextProperty); }
+            set { base.SetValue(TextProperty, value); }
+        }
+
+        public bool IsEditing
+        {
+            get { return _isEditing; }
+            set { SetPropertyField(ref _isEditing, value); }
+        }
+
+        public string ChangedText
+        {
+            get { return _changedText; }
+            set { SetPropertyField(ref _changedText, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(EditableLabel), new PropertyMetadata(""));
+
+
+        private void Text_OnDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ChangedText = Text;
+            IsEditing = true;
+            EditingBox.Focus();
+        }
+
+        private void Text_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Text = ChangedText;
+                IsEditing = false;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                ChangedText = Text;
+                IsEditing = false;
+            }
+        }
+
+        protected void SetPropertyField<T>(ref T field, T newValue, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+}
